4a96a6081e4170cfac25170f105ab6881695680f
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-15 07:45:45 -0700

    fix(express)
    serverwas looking for nonextant folder

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const dist = "/dist",
-  docs = "/docs",
+  // docs = "/docs",
   port = process.env.PORT || 5000;
 
 let app = require("express"),
   exp = exp();
 
 app.use("/", exp.static(__dirname + dist));
-app.use("/docs", exp.static(__dirname + docs));
+// app.use("/docs", exp.static(__dirname + docs));
 
 app.listen(port);
 
