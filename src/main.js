1118d71f9ffccb040510d89b529e16dc39f1cdb5
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-15 12:45:18 -0700

    chore: alias WP API to window var

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
+import App from "./App.vue";
+
+import API from "@/VuePress/WP.API";
+API.then(WP => (window.WP = WP));
 
 import "@/includes/use/log";
 import "@/includes/use/async";
-import router from "@/includes/use/router";
 
-import App from "./App.vue";
+import router from "@/includes/use/router";
 
 new Vue({
   router,
