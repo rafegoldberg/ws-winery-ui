f918c978a85d8cc970cbed09503a7e6fd48c83f3
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-15 07:38:15 -0700

    add: Vue config

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
+    devServer: {
+      host: "0.0.0.0"
+    },
+    resolve: {
+      alias: {}
+    }
+  },
+
+  css: {
+    extract: false
+  },
+  lintOnSave: true
+};
