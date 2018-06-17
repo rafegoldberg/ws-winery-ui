7f27ffeec0994a80db9e40dbb97216ecf8e67e44
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-17 11:10:28 -0700

    format(WP)

--- index.php
+++ index.php
@@ -2,31 +2,34 @@
 <html lang="en">
 <head>
 
-  <title>Berkley Journal of Gender Law &amp; Justice</title>
+  <title>VuePress</title>
 
   <!-- Metatags -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   
-  <!-- Dependencies -->
+  <!-- External -->
   <script src="//unpkg.com/wpapi@1.1.2/browser/wpapi.min.js"></script>
   <script src="//unpkg.com/vue"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   
   <!-- Wordpress -->
-  <? wp_head() ?>
+  <?wp_head()?>
+  
 </head>
-<body <? body_class() ?>>
+<body <?body_class()?>>
 
-  <main id="app">
-    <? if (have_posts()) while (have_posts()) {
-      the_post();
-      the_title();
-      the_content();
-    } ?>
-  </main>
+  <!-- Vue -->
+  <div id="app">
+  <?if (have_posts()) while (have_posts()) {
+    the_post();
+    the_title();
+    the_content();
+  }?>
+  </div>
 
-<? wp_footer() ?>
+  <!-- Wordpress -->
+  <?wp_footer()?>
+
 </body>
 </html>
