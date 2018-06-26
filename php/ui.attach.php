b4730fb6eba185bfbe729d4f98e34d8b1689bff0
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-25 17:05:53 -0700

    fix ui wp enqueue

--- php/ui.attach.php
+++ php/ui.attach.php
@@ -2,10 +2,11 @@
    * Link UI Scripts & Styles
    */
   function LinkUiAssets(){
-    $BUNDLE = get_template_directory_uri() . "/dist/app";
+    $BUNDLE = get_template_directory_uri() . "/dist";
 
-    // wp_enqueue_style(  'UiStyles', "$BUNDLE.css"  );
-    wp_enqueue_script( 'UiScript', "$BUNDLE.js", [], false, true );
+    // wp_enqueue_style(  'UiStyles', "$DIST/filename.css"  );
+    wp_enqueue_script( 'UiScript', "$DIST/app.js", [], false, true );
+    wp_enqueue_script( 'UiVendor', "$DIST/chunk-vendors.js", [], false, true );
     
     wp_localize_script('UiScript','WP_API_Settings',[
       'endpoint' => esc_url_raw(rest_url()),
