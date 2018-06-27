e4ac329f78c38380e71ca8706ee9887c12b89bf1
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-27 06:21:37 -0700

    BUILD

--- php/api.php
+++ php/api.php
@@ -1,18 +1,16 @@
 <?
-function API_EnableCrossOrigin()
-{
-  if (!did_action('rest_api_init') && $_SERVER['REQUEST_METHOD'] == 'HEAD') {
+function API_EnableCrossOrigin(){
+  if( !did_action('rest_api_init') && $_SERVER['REQUEST_METHOD'] == 'HEAD' ){
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Expose-Headers: Link');
     header('Access-Control-Allow-Methods: HEAD, GET, OPTIONS');
   }
-};
-add_action('send_headers', 'API_EnableCrossOrigin');
+}
+add_action( 'send_headers', 'API_EnableCrossOrigin' );
 ?>
 
 <?
-function API_EnableMetaQuery($valid_vars)
-{
+function API_EnableMetaQuery($valid_vars){
   $valid_vars = array_merge($valid_vars, array('filter'));
   return $valid_vars;
 }
--- src/VuePress/WP.js
+++ src/VuePress/WP.js
@@ -8,7 +8,7 @@ import WpRest from "wpapi";
 const URI =
   typeof WP_API_Settings == "object" ?
   WP_API_Settings.endpoint :
-  "http://192.168.64.2/clients/BGLJ/",
+  "http://192.168.64.2/clients/ws3/",
   API = WpRest.discover(URI);
 
 /**
