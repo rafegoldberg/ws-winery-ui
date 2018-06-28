e848b94f958b439b7e3ea556d91c8521ba3bfa09
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-28 08:11:19 -0700

    ref(views): update posts/Wine template

--- src/views/Posts/Wine.vue
+++ src/views/Posts/Wine.vue
@@ -1,16 +1,18 @@
 <template>
-<VPItem :id="id" class="WinePost UiTheme_dark">
+<VPItem :id="id" class="WinePost UiTheme_cream">
     
   <template slot-scope="WP" v-if="!WP.loading">
     
-      <header>
-        <UiHeading :level="2">Wine Post</UiHeading>
-        <UiHeading :level="3" :scale="4">{{WP.title.rendered}}</UiHeading>
-      </header>
-      <hr>
-      <div>
-        {{category}}{{hierarchy && "/" + hierarchy.replace('-',' ')}}
-      </div>
+    <UiHeading :level="1" :scale="2">{{WP.title.rendered}}</UiHeading>
+
+    <hr>
+    
+    <div>
+      <UiHeading :level="3" :scale="5">Hierarchy:</UiHeading>
+      <ul class="WinePost--hierarchy" style="margin:0">
+        <li class="WinePost--category" v-for="cat in [category,...(hierarchy && hierarchy.replace('-',' ').split('/'))]">{{cat}}</li>
+      </ul>
+    </div>
     
     <pre>{{WP}}</pre>
     {{$log({WP})}}
@@ -37,26 +39,53 @@ export default {
 
 <style lang="scss" scoped>
 .WinePost {
+  &--hierarchy {
+    list-style: none;
+  }
+  &--category {
+    display: inline-block;
+    text-transform: capitalize;
+
+    font-size: .88em;
+    font-weight: 100;
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+
+    & + &:before {
+      content: "/";
+      display: inline-block;
+      margin: 0 .3em;
+    }
+  }
+}
+</style>
+<style lang="scss">
+.WinePost {
+  box-sizing: border-box;
   max-width: 100vw;
   min-height: 100vh;
-  /deep/ > *:not(hr) {
+  padding: 1.5rem 0;
+  > *:not(hr) {
     margin: 1.5rem;
     &:first-child {
       margin-top: 0;
-      padding-top: 1.5rem;
     }
     &:last-child { margin-bottom: 0 }
   }
-  > hr { margin-left: 1.5rem }
-  /deep/ pre {
+  > hr {
+    margin-left: 1.5rem;
+    opacity: .25;
+  }
+  pre {
     display: block;
     overflow: scroll;
     max-width: 100%;
     white-space: pre;
     padding: .8em 1em;
     margin-right: 0;
-    background-color: rgba(#fff,.063);
-    max-height: 68vh;
+    background-color: rgba(#BDBDBD,.1);
+    max-height: 63vh;
+    border-radius: .5em 0 0 .5em;
   }
 }
 </style>
