a6f1545db61a917364e8673636d2e142b06d5334
Rafe Goldberg <rafegoldberg@gmail.com>
2018-07-04 08:41:42 -0700

    fix(router): uncustomize prod history base URL

--- src/includes/use/router.js
+++ src/includes/use/router.js
@@ -4,9 +4,7 @@ Vue.use(Router);
 
 import routes from "@/routes";
 export default new Router({
-  base: process.env.NODE_ENV === 'production'
-    ? '/clients/ws3/'
-    : '/',
+  base: '/', // process.env.NODE_ENV === 'production' ? '/clients/ws3/' : '/',
   routes,
   mode: "history",
   linkActiveClass: "open",
