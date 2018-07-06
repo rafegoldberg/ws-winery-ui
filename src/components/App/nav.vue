b7fc0b3f1ba5a491eb7e161a2f7ab1b78319ba8c
Rafe Goldberg <rafegoldberg@gmail.com>
2018-07-06 06:27:05 -0700

    fix(app/nav): nav expands to full width, no wrap

--- src/components/App/nav.vue
+++ src/components/App/nav.vue
@@ -1,6 +1,5 @@
 <template>
 <nav id="AppNav">
-<div id="AppNavWrap">
 
   <router-link to="/">
     <UiIcon id="AppNav-logo" name="logo" height="auto" width="9rem" :view="[0,0,180,45.71]"/>
@@ -12,8 +11,7 @@
     </button>
     <AppMenu @click.stop ref="menu"/>
   </span>
-  
-</div>
+
 </nav>
 </template>
 
@@ -38,29 +36,25 @@ export default {
 @import "~@/styles/extend/wrap";
 #AppNav {
   & {
-    position: sticky;
-    z-index: 9;
-    top: 0;
-    left: 0;
-
-    display: flex;
-    align-items: center;
-    width: 100vw;
-    height: 6rem;
-    padding: 0 1em;
-    
+    & { // outer layout
+      position: sticky;
+      z-index: 9;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 6rem;
+    }
+    & { // inner layout
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1.5rem;
+    }
     user-select: none;
   }
   &[class*="UiTheme_light"],
   &[class*="UiTheme_cream"]{ color: Color(alt) }
   &[class*="UiTheme_dark"] { color: Color(theme) }
   
-  &Wrap{
-    @extend %wrap, %wrap_flex;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 0;
-  }
 }
 </style>
