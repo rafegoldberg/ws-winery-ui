dd6ba8120f5b9e30f58759b386c24aec0afa53ea
Rafe Goldberg <rafegoldberg@gmail.com>
2018-07-09 09:09:16 -0700

    ref(main)

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
+import API from "@/VuePress/WP"
 import Vue from "vue"
-import App from "./App.vue"
-import router from "@/includes/use/router"
+
+Vue.config.devtools = true
+Vue.config.productionTip = false
 
 import "@/includes/use/log"
 import "@/includes/use/async"
 import "@/includes/use/scrollView"
-
 import "@/includes/filters"
 
-import API from "@/VuePress/WP"
+import App from "./App.vue"
+import router from "@/includes/use/router"
 
 new Vue({
   router,
   render: h=> h(App)
 }).$mount("#app")
 
-Vue.config.devtools = true
-Vue.config.productionTip = false
