d6ce95755868b6657e20ded8e1ba02acc3019912
Rafe Goldberg <rafegoldberg@gmail.com>
2018-07-17 11:20:12 -0700

    style(views/people): add role notation to bio page templates

--- src/views/People/Bio.vue
+++ src/views/People/Bio.vue
@@ -1,11 +1,13 @@
 <template>
-<main v-if="!context.loading" id="Bio" class="UiTheme_cream">
+<main v-if="!context.loading" class="Bio  UiTheme_cream">
 
   <UiPanel class="UiTheme_cream wrap_mid" connect="down">
-    <UiBox>
+    <UiBox class="Bio--overview">
       <div :style="{
+        position: 'relative',
         flex:[ 0, '50%' ],
         }">
+        <span class="Bio--role" v-html="context.acf.role.name"/>
         <img :src="featured_img"
           :alt="context.title.rendered"
           :style="{
@@ -18,10 +20,12 @@
           flex:[ 0, '50%' ],
           paddingLeft: '2em',
           }">
-        <UiHeading :level="2" :scale="3" class="UiHeading_gold">
-          {{context.title.rendered}}
-        </UiHeading>
-        <b class="Bio--position">{{context.acf.position}}</b>
+        <header>
+          <UiHeading :level="2" :scale="3" class="UiHeading_gold">
+            {{context.title.rendered}}
+          </UiHeading>
+          <b class="Bio--position">{{context.acf.position}}</b>
+        </header>
         <p>{{context.acf.intro}}</p>
         <ReadMore href="#content" class="ReadMore_gold" :icon="false"/>
       </div>
@@ -36,7 +40,7 @@
       <div>
         <article v-html="context.acf.content"/>
         <br>
-        <router-link to="/people" style="text-decoration: none">
+        <router-link to="/people" style="display: block; text-align: center; text-decoration: none">
           <UiButton class="UiButton_outline UiButton_gold UiButton_flex">
             <UiIcon name="ArrowRight" width="1em" height="1em"/>&nbsp; All Our People
           </UiButton>
@@ -93,6 +97,37 @@ export default {
 .Bio {
   &--position {
     text-transform: capitalize;
+  }
+  &--role {
+    display: inline-block;
+    font-size: 1.3rem;
+    font-weight: 100;
+    text-transform: uppercase;
+    transform: rotate(90deg) translateX(2.6rem);
+    transform-origin: left top;
+    letter-spacing: 0.15em;
+    @include Break( max-width Breaks(3) ){
+      font-size: 1.1rem;
+      transform: rotate(90deg) translateX(2.3rem);
+    }
+  }
+  &--overview {
+    @include Break( max-width Breaks(3) ){
+      flex-flow: nowrap column;
+      padding-top: 8.5rem !important;
+      >:first-child {
+        min-width: 100%;
+        margin-bottom: 1.5rem;
+      }
+      >:last-child {
+        padding-left: 0 !important;
+        & > :not(p) { text-align: center !important }
+      }
+      img:only-child {
+        min-width: 100%;
+        max-height: 22em;
+      }
+    }
   }
 }
 </style>
