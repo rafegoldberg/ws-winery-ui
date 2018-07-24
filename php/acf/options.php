c620214ca3734350abed540338ea519dce8daebc
Rafe Goldberg <rafegoldberg@gmail.com>
2018-07-24 10:18:29 -0700

    fix(wp): acf options page

--- php/acf/options.php
+++ php/acf/options.php
@@ -1,13 +1,12 @@
 <?if( function_exists('acf_add_options_page') ):
 
   acf_add_options_page([
-    "page_title"     => 'Williams Selyem Theme Options',
-    "menu_title"     => '<small>Williams<br>Selyem</small>',
-    "icon_url"       => 'http://premiumwines.com.br/images/ico_guarda_1.jpg',
-
     "menu_slug"      => 'ws-options',
-    // "parent_slug"    => 'appearance',
     
+    "page_title"     => 'Williams Selyem Theme Options',
+    "menu_title"     => '<small style="display: block; line-height: 1.1; font-weight: bold; margin-bottom: -8px; margin-top: -4px; border-top: 1px solid transparent; border-bottom: 4px solid transparent;">Williams<br>Selyem</small>',
+    "icon_url"       => 'dashicons-awards', //'http://premiumwines.com.br/images/ico_guarda_1.jpg',
+
     "update_button"  => 'Save Options',
     "update_message" => 'Theme options were saved!',
 
@@ -17,26 +16,3 @@
   ]);
 
 endif?>
-
-<?if( is_admin() ):?>
-<style>
-#adminmenu .wp-menu-name > small {
-  display: block;
-  line-height: 1.1;
-  font-weight: bold;
-  margin-bottom: -8px;
-  margin-top: -4px;
-  border-top: 1px solid transparent;
-  border-bottom: 4px solid transparent;
-}
-#adminmenu img[src="http://premiumwines.com.br/images/ico_guarda_1.jpg"] {
-  width: 2.2em;
-  margin-top: -0.55em;
-  mix-blend-mode: screen;
-  filter: invert(1) contrast(2);
-}
-#adminmenu .current img[src="http://premiumwines.com.br/images/ico_guarda_1.jpg"] {
-  filter: invert(1) contrast(8);
-}
-</style>
-<?endif?>
