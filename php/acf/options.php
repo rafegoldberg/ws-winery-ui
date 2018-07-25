e95eac1d20bb4edb29eeed53ecabe8c4b9305151
Rafe Goldberg <rafegoldberg@gmail.com>
2018-07-25 06:34:38 -0700

    ref(wp): rename site admin options tab to "Site"

--- php/acf/options.php
+++ php/acf/options.php
@@ -4,8 +4,8 @@
     "menu_slug"      => 'ws-options',
     
     "page_title"     => 'Williams Selyem Theme Options',
-    "menu_title"     => '<small style="display: block; line-height: 1.1; font-weight: bold; margin-bottom: -8px; margin-top: -4px; border-top: 1px solid transparent; border-bottom: 4px solid transparent;">Williams<br>Selyem</small>',
-    "icon_url"       => 'dashicons-awards', //'http://premiumwines.com.br/images/ico_guarda_1.jpg',
+    "menu_title"     => 'Site',
+    "icon_url"       => 'dashicons-image-filter', //'http://premiumwines.com.br/images/ico_guarda_1.jpg',
 
     "update_button"  => 'Save Options',
     "update_message" => 'Theme options were saved!',
