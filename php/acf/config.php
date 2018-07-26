0de7a4586a2075d4c88f8f9dacb8b08a3f6f5c8b
Rafe Goldberg <rafegoldberg@gmail.com>
2018-07-26 16:31:45 -0700

    feat: enable google maps api

--- php/acf/config.php
+++ php/acf/config.php
@@ -2,6 +2,13 @@
 // display default WP custom fields metabox
 add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
 
+// Google Maps API authentication
+add_filter('acf/fields/google_map/api',function($API){
+	$API['key'] = 'AIzaSyA3ozyFU65U2MA4v9j6tJhz1R28MQjJ-qc';
+	return $API;
+});
+
+
 // hide the default ACF options page
 add_action('admin_menu',function(){
   remove_menu_page('acf-options');
