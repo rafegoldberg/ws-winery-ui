7eee25912606f13deca91381b81627266d75de66
Rafe Goldberg <rafegoldberg@gmail.com>
2018-08-09 15:42:08 -0700

    feat(AppLoad)

---
+++ src/components/App/load.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="AppLoad">
+    <UiPanel class="UiTheme_cream">
+      <UiBox class="UiBox_stack">
+        <img :src="spinner">
+        Loading...
+      </UiBox>
+    </UiPanel>
+  </div>
+</template>
+<script>
+import UiPanel from '@/components/UI/Panel'
+import UiBox   from '@/components/UI/Box'
+import spinner from '@/assets/preloader.gif'
+export default {
+  name: "Loading",
+  components:{ UiPanel, UiBox },
+  data:()=>({ spinner })
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/theme/breaks";
+#AppLoad {
+  .UiBox {
+    min-height: calc( 100vh - 4rem);
+    @include Break( max-width Breaks(4) ){
+      margin-top: -4rem;
+      padding-top: 9rem;
+    }
+    justify-content: center;
+    align-self: stretch;
+  }
+}
+</style>
