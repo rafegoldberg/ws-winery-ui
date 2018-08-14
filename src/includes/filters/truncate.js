56cbb0f8dcd53e640442410dc2813ca907a6d936
Rafe Goldberg <rafegoldberg@gmail.com>
2018-08-14 15:52:43 -0700

    ref(filters/truncate): clean HTML before truncation

--- src/includes/filters/truncate.js
+++ src/includes/filters/truncate.js
@@ -2,10 +2,17 @@
  * Vue filter to truncate a string to the specified length.
  * @param {String} value The value string.
  */
-export function truncate( value, length=170, affix='…' ){
+export function truncate(value,length=170,opt={
+  affix:'…',
+  cleanHTML: true
+}){
   if( !value ) return
   if( value.length < length ) return value
 
+  opt.cleanHTML && (
+    value = value.trim().replace(/(<([^>]+)>)/ig,"")
+    )
+
   length = length - 3
-  return value.substring(0,length).trim() + affix
+  return value.substring(0,length).trim() + opt.affix
 }
