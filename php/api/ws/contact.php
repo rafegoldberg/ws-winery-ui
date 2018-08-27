d657662112035fb32d97b123db3a39540ff51925
Rafe Goldberg <rafegoldberg@gmail.com>
2018-08-27 09:58:59 -0700

    ref(wp/api/ws/contact): test html email

--- php/api/ws/contact.php
+++ php/api/ws/contact.php
@@ -4,16 +4,38 @@ function ws_api_contact(WP_REST_Request $req){
   $query = $req->get_query_params();
 
   $send  = "rafegoldberg@gmail.com";
+  $email = $req->get_param('email');
   $name  = $req->get_param('name');
   $subj  = $req->get_param('subject');
-  $email = $req->get_param('email');
   $text  = $req->get_param('text');
-  $text .= "<br><br>$name <$email>";
+
+  ob_start();
+    ?>
+    <table>
+      <tr>
+        <th style="text-align:right">From:</th>
+        <td>
+          <?=$name?> <em>(<?=$email?>)</em>
+        </td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Subject:</th>
+        <td><?=$subj?></td>
+      </tr>
+      <tr>
+        <th style="text-align:right">Message:</th>
+        <td>
+          <p><?=$text?></p>
+        </td>
+      </tr>
+    </table>
+    <?
+  $html = ob_get_clean();
 
   if( in_array(false, $query) )
   return new WP_Error('wpse-error', esc_html__('Please fill out all fields.','wpse'), ['status'=>404]);
   
-  $mail = wp_mail($send, $subj, $text);
+  $mail = wp_mail($send, $subj, $htm, ['Content-Type: text/html; charset=UTF-8']);
   
   return new WP_REST_Response(['query'=>[
     'send'=> $send,
