cbac87022552a27545747dcae4f925a9d93ff8ec
Rafe Goldberg <rafegoldberg@gmail.com>
2018-08-27 15:12:58 -0700

    ref(ContactForm)

--- php/api/ws/contact.php
+++ php/api/ws/contact.php
@@ -3,7 +3,7 @@ function ws_api_contact(WP_REST_Request $req){
 
   $query = $req->get_query_params();
 
-  $send  = "rafegoldberg@gmail.com";
+  $send  = "contact@williamsselyem.com";
   $email = $req->get_param('email');
   $name  = $req->get_param('name');
   $subj  = $req->get_param('subject');
@@ -12,27 +12,43 @@ function ws_api_contact(WP_REST_Request $req){
   if( in_array(false, $query) | in_array("false", $query)  )
     return new WP_Error('missing_params', __("All fields are required. Make sure you've filled ever out the form and <a href='?'>try again</a>!"), ['status'=>405]);
 
+  $img = get_field('fallbacks','options')['mailer_image'];
   ob_start();
   ?>
     <table style="border-collapse: collapse">
       <tr>
-        <th style="text-align:right; padding: 10px; vertical-align: top; border-right: 1px solid #DDD">From:</th>
-        <td style="padding: 10px">
+        <td colspan="2" align="center">
+          <a href="<?=get_site_url()?>">
+            <img src="<?=$img?>" alt="<?=get_bloginfo('name')?>" style="width: 180px">
+          </a>
+        </td>
+      </tr>
+    </table>
+    <table style="border-collapse: collapse">
+      <tr>
+        <th style="text-align:right; padding: 10px; vertical-align: top; border-right: 1px solid #DDD; border-bottom: 1px solid #BA9454">From:</th>
+        <td style="padding: 10px; border-bottom: 1px solid #BA9454">
           <?=$name?> <em>(<?=$email?>)</em>
         </td>
       </tr>
       <tr>
-        <th style="text-align:right; padding: 10px; vertical-align: top; border-right: 1px solid #DDD">Regarding:</th>
-        <td style="padding: 10px"><?=$subj?></td>
+        <th style="text-align:right; padding: 10px; vertical-align: top; border-right: 1px solid #DDD; border-bottom: 1px solid #BA9454">Regarding:</th>
+        <td style="padding: 10px; border-bottom: 1px solid #BA9454"><?=$subj?></td>
       </tr>
       <tr>
         <th style="text-align:right; padding: 10px; vertical-align: top; border-right: 1px solid #DDD">Message:</th>
         <td style="padding: 10px"><?=$text?></td>
       </tr>
     </table>
-    <hr>
-    <p style="color: #666">
-      <em>Hit <b>Reply</b> to respond directly to the user via email.</em>
+    <hr style="border:solid #DDD; border-width:1px 0 0">
+    <p style="color: #666; line-height: 1.3">
+      <small>
+        This email was sent from
+        <a href="<?=get_site_url()?>" style="font-style: italic; font-weight: normal">
+          <?=get_bloginfo('name')?>
+        </a>.
+        You can <b>reply to this message to email the user</b> back (at <?=$email?>.)
+      </small>
     </p>
   <?
   $html = ob_get_clean();
@@ -43,10 +59,11 @@ function ws_api_contact(WP_REST_Request $req){
   ];
   $mail = wp_mail($send, "Williams Selyem – $subj", $html, $head);
   
-  if( !$mail)
+  if( !$mail )
     return new WP_Error('couldnt_send', __("We hit a snag sending your message! Please <a href='?'>try again</a> in a bit, or reach us by phone at the number below."), ['status'=>501]);
   
   return new WP_REST_Response([
+    'markup' => $html,
     'message'=> "We'll get back to you as soon as we can."
   ], 200);
 }
