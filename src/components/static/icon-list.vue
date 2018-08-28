deabc7e3a51eceefaf69b4b4f111ffd260f269d9
Rafe Goldberg <rafegoldberg@gmail.com>
2018-08-28 07:36:35 -0700

    feat(IconList): generated markup from data, props

--- src/components/static/icon-list.vue
+++ src/components/static/icon-list.vue
@@ -1,32 +1,45 @@
 <template>
-<section class="iconList">
-  
-  <div class="iconList--item">
-    <UiIcon name="bottles" width="3rem" height="3rem"/>
-    <div class="iconList--item--text">
-      <span class="iconList--item--label">All Our Wines</span>
-      <div>Visit our wine library to find a specific bottle or peruse our full collection.</div>
-    </div>
-    <router-link class="UiButton" to="/wine#all">Wine Library</router-link>
-  </div>
-  <div class="iconList--item">
-    <UiIcon name="barrels" width="3rem" height="3rem"/>
-    <div class="iconList--item--text">
-      <span class="iconList--item--label">How To Purchase</span>
-      <div>Our wines are available twice a year for purchase.</div>
-    </div>
-    <router-link class="UiButton" to="/purchase">How to Buy</router-link>
-  </div>
-  <div class="iconList--item">
-    <UiIcon name="glasses" width="3rem" height="3rem"/>
-    <div class="iconList--item--text">
-      <span class="iconList--item--label">Visit The Winery</span>
-      <div>Arrange a private tour and tasting tailored specifically for you.</div>
-    </div>
-    <router-link class="UiButton" to="/visit">Visit Us</router-link>
-  </div>
 
-</section>
+  <section class="iconList">
+
+    <div class="iconList--item" v-for="(item, key) in items" :key="key">
+      {{$log({item,key})}}
+      <UiIcon :name="item.icon || key" width="3rem" height="3rem"/>
+      <div class="iconList--item--text">
+        <span class="iconList--item--label" v-html="item.title"/>
+        <div v-html="item.text"/>
+      </div>
+      <router-link class="UiButton" :to="item.URL" v-text="item.CTA"/>
+    </div>
+
+    <!-- 
+      <div class="iconList--item">
+        <UiIcon name="bottles" width="3rem" height="3rem"/>
+        <div class="iconList--item--text">
+          <span class="iconList--item--label">All Our Wines</span>
+          <div>Visit our wine library to find a specific bottle or peruse our full collection.</div>
+        </div>
+        <router-link class="UiButton" to="/wine#all">Wine Library</router-link>
+      </div>
+      <div class="iconList--item">
+        <UiIcon name="barrels" width="3rem" height="3rem"/>
+        <div class="iconList--item--text">
+          <span class="iconList--item--label">How To Purchase</span>
+          <div>Our wines are available twice a year for purchase.</div>
+        </div>
+        <router-link class="UiButton" to="/purchase">How to Buy</router-link>
+      </div>
+      <div class="iconList--item">
+        <UiIcon name="glasses" width="3rem" height="3rem"/>
+        <div class="iconList--item--text">
+          <span class="iconList--item--label">Visit The Winery</span>
+          <div>Arrange a private tour and tasting tailored specifically for you.</div>
+        </div>
+        <router-link class="UiButton" to="/visit">Visit Us</router-link>
+      </div>
+    -->
+  </section>
+
 </template>
 
 <script>
@@ -35,6 +48,45 @@ import UiButton from "@/components/UI/Button"
 
 export default {
   name: "StaticIconList",
+  created(){
+    if( Object.keys(this.list).length )
+      this.items = Object.assign({}, this.items, this.list)
+  },
+  props:{
+    list: {
+      type: Object,
+      default:()=>({
+        glasses: {
+          title:"Visit The Winery",
+          text: "Arrange a private tour and tasting tailored specifically for you.",
+          CTA:  "Visit Us",
+          URL:  '/visit',
+        }
+      })
+    }
+  },
+  data:()=>({
+    items: {
+      bottles: {
+        title:"All Our Wines",
+        text: "Visit our wine library to find a specific bottle or peruse our full collection.",
+        CTA:  "Wine Library",
+        URL:  '/wine#all',
+      },
+      barrels: {
+        title:"How To Purchase",
+        text: "Our wines are available twice a year for purchase.",
+        CTA:  "How to Buy",
+        URL:  '/purchase',
+      },
+      /* glasses: {
+        title:"Visit The Winery",
+        text: "Arrange a private tour and tasting tailored specifically for you.",
+        CTA:  "Visit Us",
+        URL:  '/visit',
+      } */
+    }
+  }),
   components:{ UiIcon, UiButton }
 }
 </script>
@@ -46,16 +98,16 @@ export default {
 @import "~@/components/UI/Box/style";
 
 .iconList {
-  min-width: 68%;
+  min-width: 66%;
   margin: 0 auto;
   &--item {
     margin: 0 auto;
     display: flex;
     align-items: center;
     & + & {
-      margin-top: 1rem;
-      padding-top: 1rem;
-      border-top: 1px solid Color(theme);
+      margin-top: 1.9rem;
+      padding-top: 1.9rem;
+      border-top: 1px solid rgba(Color(theme),.5);
     }
     > * {
       margin: .5rem 2rem;
@@ -63,13 +115,17 @@ export default {
     }
     &--label {
       text-transform: uppercase;
-      font-weight: 600;
+      font-weight: 900;
       color: Color(theme);
-      letter-spacing: 0.1em;
+      letter-spacing: .05em;
     }
   }
   .UiButton {
     white-space: nowrap;
+    text-align: center;
+    @include Break( min-width Breaks(2) ){
+      min-width: 145px;
+    }
   }
   /deep/ .UiIcon {
     margin: 0;
@@ -83,21 +139,28 @@ export default {
 }
 
 .iconList {
+  @include Break( min-width Breaks(3) ){
+    &--item--label {
+      display: block;
+      font-size: 1.15em;
+      margin-bottom: -0.3em;
+    }
+  }
   @include Break( max-width Breaks(2) ){
     &--item {
       flex-flow: wrap row;
       >svg {
         align-self: flex-start !important;
-        flex: 1 0 2.5rem;
-        width: 2.5rem !important;
-        height: 2.5rem !important;
+        flex: 1 0 2.6rem;
+        width: 2.6rem !important;
+        height: 2.6rem !important;
       }
       &--label {
         display: block;
         margin: .5rem 0;
       }
       &--text {
-        flex: 1 calc(100% - 2.5rem);
+        flex: 1 calc(100% - 2.6rem);
         margin: 0;
         line-height: 1.75;
         padding: 0 0 0 1rem;
