083cc6562954a650bafa99834b3c698cebf954d6
Rafe Goldberg <rafegoldberg@gmail.com>
2018-09-04 16:51:13 -0700

    add(icons): location marker

---
+++ src/icons/marker.vue
@@ -0,0 +1,26 @@
+<template>
+  <g id="icon" class="icon" data-name="Marker" transform="translate(-1371 -3046.7)">
+    <g data-name="Large Pin">
+      <path fill="none" d="M1371 3046.7h54.4v54.4H1371z" data-name="Rectangle 305"/>
+      <path fill="#ba9454" fill-rule="evenodd" d="M1396.6 3084a10.9 10.9 0 0 0 11-11c0-5.8-4.7-11.3-11-11.3a10.9 10.9 0 0 0-10.9 11c0 5.8 5.1 11.2 11 11.2zm-18.3-29.7a25.9 25.9 0 0 1 36.7 0 26 26 0 0 1-1 36.7l-17.4 21-17.4-21c-9.7-10.2-10.6-26.6-.9-36.7z" data-name="Path 114"/>
+    </g>
+    <circle cx="3.7" cy="3.7" r="3.7" fill="#ba9454" data-name="Ellipse 7" transform="translate(1393.2 3069.5)"/>
+  </g>
+</template>
+
+<script>
+export default {
+  name:"IconMarker",
+  data:()=>({
+    viewBox: [ 0, 0, 63.4, 48 ]
+  })
+}
+</script>
+
+<style scoped>
+.lightFill{
+  fill: none;
+  stroke: #fff;
+  stroke-miterlimit: 10;
+}
+</style>
