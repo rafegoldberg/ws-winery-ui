89788c9f9ddc57cfbb61007674c781e558bab55b
Rafe Goldberg <rafegoldberg@gmail.com>
2018-09-06 14:17:54 -0700

    ref(acf/options): programmatic option pages registration

--- php/acf/options.php
+++ php/acf/options.php
@@ -3,19 +3,24 @@
   // Configure Site
   // Options Page
   // 
-  acf_add_options_page([
-    "menu_slug"      => 'ws-options',
-    
-    "page_title"     => 'Williams Selyem Theme Options',
-    "menu_title"     => 'Site',
-    "icon_url"       => 'dashicons-image-filter',
-
-    "update_button"  => 'Update Site',
-    "update_message" => 'Site settings saved!',
-
-    "capability"     => 'edit_posts',
-    "position"       => -1,
-    "redirect"       => false,
-  ]);
+  $test = [
+    [ "page_title" => 'Listing Pages',
+      "menu_slug" => 'ws-test',
+      "menu_title" => 'Listings',
+      "icon_url" => 'dashicons-editor-ol',
+      "position" => -2,
+      ],
+    [ "page_title" => 'Williams Selyem Theme Options',
+      "menu_slug" => 'ws-options',
+      "menu_title" => 'Site',
+      "icon_url" => 'dashicons-image-filter',
+      "update_button" => 'Update Site',
+      "update_message" => 'Site settings saved!',
+      "capability" => 'edit_posts',
+      "position" => -1,
+      "redirect" => false,
+    ],
+  ];
+  array_map('acf_add_options_page',$test);
 
 }?>
