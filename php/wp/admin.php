5242985c584c921ae10419a32425601a72d6e9b7
Rafe Goldberg <rafegoldberg@gmail.com>
2018-09-06 14:17:08 -0700

    style(wp)

--- php/wp/admin.php
+++ php/wp/admin.php
@@ -1,7 +1,9 @@
 <?
-function wp_admin_styles() {
-  wp_register_style('ws_wp_css', get_template_directory_uri()."/admin/style.css", false, '1.0.0');
-  wp_enqueue_style('ws_wp_css');
+function wp_admin_styles(){
+  $root = get_template_directory_uri();
+  $file = 'admin.css';
+  wp_register_style('ws_wp_css', "$root/$file", false, '1.0.0');
+  wp_enqueue_style( 'ws_wp_css' );
 }  
-add_action( 'admin_enqueue_scripts', 'wp_admin_styles' );
+add_action('admin_enqueue_scripts', 'wp_admin_styles');
 ?>
