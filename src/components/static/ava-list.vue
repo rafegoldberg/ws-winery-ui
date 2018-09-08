2c402edfb9ca739ff376c63f0ba88ebedd1b11c2
Rafe Goldberg <rafegoldberg@gmail.com>
2018-09-07 18:14:15 -0700

    ref: nix devlogs

--- src/components/static/ava-list.vue
+++ src/components/static/ava-list.vue
@@ -5,7 +5,6 @@
       :key="index"
       :id="term.slug"
       >
-      {{$log(term)}}
       <UiHeading :level="4" v-text="term.name" class="UiHeading_gold UiHeading_bold UiHeading_sans UiHeading_scale5 UiHeading_space÷2"/>
       <br>
       {{term.description}}
