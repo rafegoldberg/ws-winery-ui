5ef9733ed44ee1ef6aa0b0a86103d280e03332cc
Rafe Goldberg <rafegoldberg@gmail.com>
2018-12-05 09:13:03 -0800

    BUILD

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 // hack to enforce transformation to commonjs
-process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true
+// process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true
 
 module.exports = {
   presets: [["@vue/app", {useBuiltIns: 'entry'}]]
