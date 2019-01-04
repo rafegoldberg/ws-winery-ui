5a0345c41fe688836d7e8c27639d186818c33d78
Rafe Goldberg <rafegoldberg@gmail.com>
2019-01-04 10:26:54 -0800

    add is IE check

--- index.php
+++ index.php
@@ -1,47 +1,49 @@
+<?
+$isie = false;
+// $isie = htmlentities($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8');
+// $isie = preg_match('~MSIE|Internet Explorer~i', $ua) || (strpos($ua, 'Trident/7.0') !== false && strpos($ua, 'rv:11.0') !== false) ? true : false;
+if( $_REQUEST['isie'] ) $isie = true;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <title>Williams Selyem</title>
-
-  <!-- Metatags -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   
-  <!-- External Dependencies -->
-  <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js"></script> -->
-  <script src="//unpkg.com/wpapi@1.1.2/browser/wpapi.min.js"></script>
+  <?if( !$isie ):?>
+  <!-- External  -->
+    <script src="//unpkg.com/wpapi@1.1.2/browser/wpapi.min.js"></script>
 
-  <!-- Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-46387613-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+  <!-- Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-46387613-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-46387613-1');
+    </script>
+  <?endif?>
 
-    gtag('config', 'UA-46387613-1');
-  </script>
-
-  <!-- Wordpress -->
   <?wp_head()?>
-  
 </head>
 <body <?body_class()?>>
 
-  <!-- Vue -->
-  <div id="app">
-    <div hidden>
-    <?if (have_posts()) while (have_posts()) {
-      the_post();
-      the_title();
-      // the_content();
-    }?>
+  <?if( !$isie ):?>
+    <div id="app">
+      <div hidden>
+      <?if( have_posts() )while( have_posts() ){
+        the_post();
+        the_title();
+        }?>
+      </div>
     </div>
-  </div>
+  <?else:?>
+    your browser is out of date!
+  <?endif?>
 
-  <!-- Wordpress -->
   <?wp_footer()?>
-
 </body>
 </html>
