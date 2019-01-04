740d8b72f974cf776eb59c06ac1df3429ac7d993
Rafe Goldberg <rafegoldberg@gmail.com>
2019-01-04 15:11:01 -0800

    fix ie warning lp

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?
 $isIE = false;
 $isIE = htmlentities($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8');
-$isIE = preg_match('~MSIE|Internet Explorer~i', $ua) || (strpos($ua, 'Trident/7.0') !== false && strpos($ua, 'rv:11.0') !== false)
+$isIE = preg_match('~MSIE|Internet Explorer~i', $isIE) || (strpos($isIE, 'Trident/7.0') !== false && strpos($isIE, 'rv:11.0') !== false)
   ? true
   : false;
 if( $_REQUEST['test']=='ie' ) $isIE = true;
@@ -15,25 +15,22 @@ if( $_REQUEST['test']=='ie' ) $isIE = true;
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   
-  <?if( !$isIE ):?>
-    <!-- External  -->
-      <script src="//unpkg.com/wpapi@1.1.2/browser/wpapi.min.js"></script>
+  <!-- External  -->
+    <script src="//unpkg.com/wpapi@1.1.2/browser/wpapi.min.js"></script>
 
-    <!-- Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-46387613-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-46387613-1');
-      </script>
-  <?endif?>
+  <!-- Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-46387613-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-46387613-1');
+    </script>
 
   <?wp_head()?>
 </head>
-<body <?if(!$isIE) body_class()?>>
+<body <?body_class(($isIE ? 'is-ie' : ''))?>>
 
-  <?if( !$isIE ):?>
     <div id="app">
       <div hidden>
       <?if( have_posts() )while( have_posts() ){
@@ -42,10 +39,10 @@ if( $_REQUEST['test']=='ie' ) $isIE = true;
         }?>
       </div>
     </div>
-  <?else:
+    
+  <?if( $isIE )
     get_template_part("templates/ie", "warning");
     ?>
-  <?endif?>
 
   <?wp_footer()?>
 </body>
